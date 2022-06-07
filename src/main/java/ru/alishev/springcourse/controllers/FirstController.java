d6ec866153cdf283527ce3d0c65d0f30b0abfa1b
alami <alami@km.ru>
2022-06-08 01:34:46 +0500

    02-06 HttpervletRequest

--- src/main/java/ru/alishev/springcourse/controllers/FirstController.java
+++ src/main/java/ru/alishev/springcourse/controllers/FirstController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * @author Neil Alishev
  */
@@ -12,7 +14,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage() {
+    public String helloPage(HttpServletRequest req) {
+        String name = req.getParameter("name");
+        String surname = req.getParameter("surname");
+        System.out.println("Hi "+ name + " "+ surname);
         return "first/hello";
     }
 
