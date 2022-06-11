889297ae4f90b437d01679d28e3e4ec95d863e50
alami <alami@km.ru>
2022-06-11 15:15:30 +0500

    07-14 hibernate CRUD

--- src/main/java/ru/alishev/springcourse/models/Person.java
+++ src/main/java/ru/alishev/springcourse/models/Person.java
@@ -26,14 +26,20 @@ public class Person {
     @Column(name = "age")
     private int age;
 
+    @Column(name = "email")
+    @NotEmpty(message = "Email should not be empty")
+    @Email(message = "Email should be valid")
+    private String email;
+
     public Person() {
 
     }
 
-    public Person(int id, String name, int age) {
+    public Person(int id, String name, int age, String email) {
         this.id = id;
         this.name = name;
         this.age = age;
+        this.email = email;
     }
 
     public int getId() {
@@ -58,5 +64,13 @@ public class Person {
 
     public void setAge(int age) {
         this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
